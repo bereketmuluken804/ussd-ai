2ad95905bf1b12713a0634c4928dd1078256bdb2
M. Bereket <bmuluken804@gmail.com>
2026-09-14 17:40:03 +0300

    feat: add custome loggger for better developer experience

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Groq } from "groq-sdk";
 import cron from "node-cron";
 import https from "https";
 import { configDotenv } from "dotenv";
+import { requestLogger, logger } from "./middleware/logger.js";
 configDotenv();
 
 const app = express();
@@ -10,6 +11,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(requestLogger);
+
 
 const groq = new Groq({
 	apiKey: process.env.GROQ_API_KEY,
@@ -20,6 +24,7 @@ app.get("/health", (req, res) => {
 });
 
 app.post("/ussd", async (req, res) => {
+  logger.info('Processing USSD session turn', { text: req.body.text });
 	const { text } = req.body;
 
 	let response = "";
@@ -65,7 +70,6 @@ app.post("/ussd", async (req, res) => {
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 	const url = process.env.url;
-
 	if (url) {
 		cron.schedule("*/14 * * * *", () => {
 			console.log("Sending self-ping to keep server awake...");
